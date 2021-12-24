7c72a8007444850473ac72600610dacd9fe9c9f5
Arjun <arjundoel@gmail.com>
2021-12-24 23:12:25 +0000

    appbar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(Todo());
+
+class Todo extends StatelessWidget {
+  const Todo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Todo-App')),
+      ),
+    );
+  }
+}
